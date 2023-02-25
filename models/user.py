be3671a4fc6e09d7a555ccd3fa311a0a55018dfc
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-02-25 20:34:47 +0100

    airbnb

--- models/user.py
+++ models/user.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
+"""user class, subclass of BaseModel
 """
-Defines the User class.
-"""
+
 from models.base_model import BaseModel
+import json
 
 
 class User(BaseModel):
-    """Represent a User
-    Attributes:
-        email (str): user email
-        password (str): user password
-        first_name (str): first name
-        last_name (str): last name
-    """
+    '''subclass of BaseModel class'''
+
     email = ""
     password = ""
     first_name = ""
